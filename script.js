d72c284ca5de0b4603e60c3bb747477f150473eb
Dinakar2329 <119375895+Dinakar2329@users.noreply.github.com>
2023-05-27 10:49:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let result = document.getElementById("result");
+
+//display result when enter key is clicked
+result.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    try {
+      result.value = eval(result.value);
+      result.value = Number(result.value).toLocaleString();
+    } catch (err) {
+      alert("Invalid / Syntax Error");
+    }
+  }
+});
+
+// To display the numbers
+function display(val) {
+  result.value += val;
+}
+
+//TO clear the numbers
+function AC() {
+  result.value = "";
+}
+
+//TO calcuate the given numbers
+function calculate(num) {
+  try {
+    result.value = eval(result.value);
+    result.value = Number(result.value).toLocaleString();
+  } catch (err) {
+    alert("Invalid / Syntax Error");
+    result.value = "";
+  }
+}
+
+//TO delete last single variable
+function del() {
+  result.value = result.value.slice(0, -1);
+}
